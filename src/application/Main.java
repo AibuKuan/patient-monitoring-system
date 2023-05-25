90c915e8a7b1882831dfb9e66245556f922ea160
Aibu Kuan <kuanaibu369@gmail.com>
2023-05-25 18:39:00 +0800

    Added frames and connector/j

---
+++ src/application/Main.java
@@ -0,0 +1,14 @@
+
+package application;
+
+import gui.*;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Home home = new Home();
+        home.setLocationRelativeTo(null);
+        home.setVisible(true);
+    }
+    
+}
